08b0ee9a76c9bc540aa71ae9d565aca5374598e9
vladyslav.pustovalov <vladyslav.pustovalov@gmail.com>
2026-09-13 12:33:05 +0200

    add forcing to be in dark theme all the time

--- app/src/main/java/my/vladpustovalov/tdp/MainActivity.kt
+++ app/src/main/java/my/vladpustovalov/tdp/MainActivity.kt
@@ -1,7 +1,9 @@
 package my.vladpustovalov.tdp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +14,10 @@ import my.vladpustovalov.tdp.ui.theme.TdpTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
         setContent {
             TdpTheme {
                 AppNavGraph()
